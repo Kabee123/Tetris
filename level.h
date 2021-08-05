eb10a93cad99b61bb20977a815f94468a794e798
Kabeelan Sivamanoharan <ksivamanoharan@uwaterloo.ca>
2021-08-05 00:05:23 +0000

    Update level.h

--- level.h
+++ level.h
@@ -4,29 +4,32 @@
 
 class Level {
 	public:
-		virtual char makeBlock() const = 0;
+		virtual char makeBlock()  = 0;
 };
 
-/*class Level_0 : public Level {
+class Level0 : public Level {
+        std::string file_name;
+        int c_idx = 0;
+        public:
+        Level0(std::string file_name);
 	virtual char makeBlock() override;
-};*/
+};
 
 class Level1 : public Level {
-        char makeBlock() const override;
+        char makeBlock()  override;
 };
 
 class Level2 : public Level {
-        char makeBlock() const override;
+        char makeBlock()  override;
 };
 
 class Level3 : public Level {
-        char makeBlock() const override;
+        char makeBlock()  override;
 };
 
 class Level4 : public Level {
-        char makeBlock() const override;
+        char makeBlock()  override;
 };
-
 
 #endif
 
